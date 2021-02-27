010df0323f8cf84d1b4789ccbc26f00f2ee6825a
Kyle <kyleham@byu.edu>
2021-02-27 12:03:28 -0700

    add auto version of the program

--- ssdHealthReport-autoRun.py
+++ ssdHealthReport-autoRun.py
@@ -1,34 +1,24 @@
 #!/usr/bin/env python3
 #This scripted will use a a basic smtp mail server from google. It requires allowing less secure access. 
 #For security, create a new email account to handle the sending of emails
+#This version of the program can be used with Cron in order to run weakly checks. 
 
 import sys, os, time, smtplib, ssl, subprocess
 #This import will be a file with username/password variables stored
 import emailCred
 
+scanLength = 'long' #change scan length here to 'short' if desired
 if not os.geteuid() == 0:
     sys.exit("\nOnly root can run this script\n")
-print("\nEnter drive location (ex: sdb): ")
-driveLoc = input()
-subprocess.call(['sudo', 'smartctl', '-t', 'short', '-i', '/dev/'+driveLoc], stdout=open(os.devnull, "w"), stderr=subprocess.STDOUT)
-print("\nRunning scan. Wait 3 minutes")
-time.sleep(150)
-print("Wear leveling listed below\n")
-os.system("sudo smartctl -a /dev/"+driveLoc+" | grep \"Wear_Leveling_Count\"")
-print("\nDo you want to view more detailed results (y or n)?\n")
-userResp = input()
-userResp = userResp.strip()
-scanReportForm = ''
-if (userResp == 'y' or userResp == 'yes'):
-    scanReport = subprocess.Popen(['sudo', 'smartctl', '-a', '/dev/'+driveLoc], stdout=subprocess.PIPE)
-    scanReportForm = scanReport.stdout.read().decode()
-    print(scanReportForm)
-print("Do you want results emailed to you (y or n)?")
-userResp = input()
-userResp = userResp.strip()
-if (userResp == 'n' or userResp == 'no'):
-    sys.exit()
-
+driveLoc = 'sdb' #change drive location here
+subprocess.call(['sudo', 'smartctl', '-t', scanLength, '-i', '/dev/'+driveLoc], stdout=open(os.devnull, "w"), stderr=subprocess.STDOUT)
+if scanLength == 'long':
+    time.sleep(9000)
+else:
+    time.sleep(150)
+#save results
+scanReport = subprocess.Popen(['sudo', 'smartctl', '-a', '/dev/'+driveLoc], stdout=subprocess.PIPE)
+scanReportForm = scanReport.stdout.read().decode()
 smtp_server = "smtp.gmail.com"
 port = 587  # For starttls
 sender_email = emailCred.email
